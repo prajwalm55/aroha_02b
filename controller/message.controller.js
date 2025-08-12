ae964314965cdde29713f7376b5cdf2c0aed2c90
Prajwal M <prajwalm377@gmail.com>
2025-08-12 18:17:34 +0530

    all done

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,34 +1,229 @@
+// import { getReceiverSocketId, io } from "../SocketIO/server.js";
+// import Conversation from "../models/conversation.model.js";
+// import Message from "../models/message.model.js";
+// export const sendMessage = async (req, res) => {
+//   try {
+//     const { message } = req.body;
+//     const { id: receiverId } = req.params;
+//     const senderId = req.user._id; // current logged in user
+//     let conversation = await Conversation.findOne({
+//       members: { $all: [senderId, receiverId] },
+//     });
+//     if (!conversation) {
+//       conversation = await Conversation.create({
+//         members: [senderId, receiverId],
+//       });
+//     }
+//     const newMessage = new Message({
+//       senderId,
+//       receiverId,
+//       message,
+//     });
+//     if (newMessage) {
+//       conversation.messages.push(newMessage._id);
+//     }
+//     // await conversation.save()
+//     // await newMessage.save();
+//     await Promise.all([conversation.save(), newMessage.save()]); // run parallel
+//     const receiverSocketId = getReceiverSocketId(receiverId);
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit("newMessage", newMessage);
+//     }
+//     res.status(201).json(newMessage);
+//   } catch (error) {
+//     console.log("Error in sendMessage", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+// export const getMessage = async (req, res) => {
+//   try {
+//     const { id: chatUser } = req.params;
+//     const senderId = req.user._id; // current logged in user
+//     let conversation = await Conversation.findOne({
+//       members: { $all: [senderId, chatUser] },
+//     }).populate("messages");
+//     if (!conversation) {
+//       return res.status(201).json([]);
+//     }
+//     const messages = conversation.messages;
+//     res.status(201).json(messages);
+//   } catch (error) {
+//     console.log("Error in getMessage", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+
+//new
+
+// import { getReceiverSocketId, io } from "../SocketIO/server.js";
+// import Conversation from "../models/conversation.model.js";
+// import Message from "../models/message.model.js";
+
+// export const sendMessage = async (req, res) => {
+//   try {
+//     const { message } = req.body;
+//     const { id: receiverId } = req.params;
+//     const senderId = req.user._id; // current logged in user
+
+//     let conversation = await Conversation.findOne({
+//       members: { $all: [senderId, receiverId] },
+//     });
+
+//     if (!conversation) {
+//       conversation = await Conversation.create({
+//         members: [senderId, receiverId],
+//       });
+//     }
+
+//     const newMessage = new Message({
+//       senderId,
+//       receiverId,
+//       message,
+//     });
+
+//     if (newMessage) {
+//       conversation.messages.push(newMessage._id);
+//     }
+
+//     // await conversation.save()
+//     // await newMessage.save();
+//     await Promise.all([conversation.save(), newMessage.save()]); // run parallel
+
+//     const receiverSocketId = getReceiverSocketId(receiverId);
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit("newMessage", newMessage);
+//     }
+
+//     res.status(201).json(newMessage);
+//   } catch (error) {
+//     console.log("Error in sendMessage", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+// export const getMessage = async (req, res) => {
+//   try {
+//     const { id: chatUser } = req.params;
+//     const senderId = req.user._id; // current logged in user
+
+//     let conversation = await Conversation.findOne({
+//       members: { $all: [senderId, chatUser] },
+//     }).populate("messages");
+
+//     if (!conversation) {
+//       return res.status(201).json([]);
+//     }
+
+//     const messages = conversation.messages;
+//     res.status(201).json(messages);
+//   } catch (error) {
+//     console.log("Error in getMessage", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+// // New function to clear chat
+// export const clearChat = async (req, res) => {
+//   try {
+//     const { id: chatUser } = req.params;
+//     const senderId = req.user._id; // current logged in user
+
+//     // Find the conversation
+//     let conversation = await Conversation.findOne({
+//       members: { $all: [senderId, chatUser] },
+//     });
+
+//     if (!conversation) {
+//       return res.status(404).json({ error: "Conversation not found" });
+//     }
+
+//     // Get all message IDs from the conversation
+//     const messageIds = conversation.messages;
+
+//     // Delete all messages from the Message collection
+//     await Message.deleteMany({
+//       _id: { $in: messageIds }
+//     });
+
+//     // Clear the messages array in the conversation
+//     conversation.messages = [];
+//     await conversation.save();
+
+//     // Emit socket event to notify both users that chat was cleared
+//     const receiverSocketId = getReceiverSocketId(chatUser);
+//     const senderSocketId = getReceiverSocketId(senderId);
+
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit("chatCleared", { conversationId: conversation._id });
+//     }
+//     if (senderSocketId) {
+//       io.to(senderSocketId).emit("chatCleared", { conversationId: conversation._id });
+//     }
+
+//     res.status(200).json({ 
+//       message: "Chat cleared successfully",
+//       conversationId: conversation._id 
+//     });
+
+//   } catch (error) {
+//     console.log("Error in clearChat", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { getReceiverSocketId, io } from "../SocketIO/server.js";
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id; // current logged in user
+
     let conversation = await Conversation.findOne({
       members: { $all: [senderId, receiverId] },
     });
+
     if (!conversation) {
       conversation = await Conversation.create({
         members: [senderId, receiverId],
       });
     }
+
     const newMessage = new Message({
       senderId,
       receiverId,
       message,
     });
+
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
     // await conversation.save()
     // await newMessage.save();
     await Promise.all([conversation.save(), newMessage.save()]); // run parallel
+
     const receiverSocketId = getReceiverSocketId(receiverId);
     if (receiverSocketId) {
       io.to(receiverSocketId).emit("newMessage", newMessage);
     }
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage", error);
@@ -40,16 +235,68 @@ export const getMessage = async (req, res) => {
   try {
     const { id: chatUser } = req.params;
     const senderId = req.user._id; // current logged in user
+
     let conversation = await Conversation.findOne({
       members: { $all: [senderId, chatUser] },
     }).populate("messages");
+
     if (!conversation) {
       return res.status(201).json([]);
     }
+
     const messages = conversation.messages;
     res.status(201).json(messages);
   } catch (error) {
     console.log("Error in getMessage", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// New function to clear chat
+export const clearChat = async (req, res) => {
+  try {
+    const { id: chatUser } = req.params;
+    const senderId = req.user._id; // current logged in user
+
+    // Find the conversation
+    let conversation = await Conversation.findOne({
+      members: { $all: [senderId, chatUser] },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ error: "Conversation not found" });
+    }
+
+    // Get all message IDs from the conversation
+    const messageIds = conversation.messages;
+
+    // Delete all messages from the Message collection
+    await Message.deleteMany({
+      _id: { $in: messageIds }
+    });
+
+    // Clear the messages array in the conversation
+    conversation.messages = [];
+    await conversation.save();
+
+    // Emit socket event to notify both users that chat was cleared
+    const receiverSocketId = getReceiverSocketId(chatUser);
+    const senderSocketId = getReceiverSocketId(senderId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("chatCleared", { conversationId: conversation._id });
+    }
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("chatCleared", { conversationId: conversation._id });
+    }
+
+    res.status(200).json({ 
+      message: "Chat cleared successfully",
+      conversationId: conversation._id 
+    });
+
+  } catch (error) {
+    console.log("Error in clearChat", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- routes/message.route.js
+++ routes/message.route.js
@@ -1,9 +1,31 @@
+// import express from "express";
+// import { getMessage, sendMessage } from "../controller/message.controller.js";
+// import secureRoute from "../middleware/secureRoute.js";
+
+// const router = express.Router();
+// router.post("/send/:id", secureRoute, sendMessage);
+// router.get("/get/:id", secureRoute, getMessage);
+
+// export default router;
+
+
+
+
+// new
+
+
+
+
+
+
 import express from "express";
-import { getMessage, sendMessage } from "../controller/message.controller.js";
+import { getMessage, sendMessage, clearChat } from "../controller/message.controller.js";
 import secureRoute from "../middleware/secureRoute.js";
 
 const router = express.Router();
+
 router.post("/send/:id", secureRoute, sendMessage);
 router.get("/get/:id", secureRoute, getMessage);
+router.delete("/clear/:id", secureRoute, clearChat); // New route for clearing chat
 
 export default router;
